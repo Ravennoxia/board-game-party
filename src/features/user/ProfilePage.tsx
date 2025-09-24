dd77600654fb6ca077f67f01442bd6f52ee6c416
Ravennoxia <ravenoxia@gmail.com>
2025-09-24 19:04:23 +0200

    Profile page edits
    - Changed inputs to work with react hook form
    - Changed design so it is more mobile friendly

--- src/features/user/ProfilePage.tsx
+++ src/features/user/ProfilePage.tsx
@@ -1,13 +1,38 @@
 import type {BGUser} from "../types.ts"
 import "./ProfilePage.css"
-import {useEffect, useState} from "react"
+import {type Dispatch, type SetStateAction, useState} from "react"
 import {doc, type Firestore, updateDoc} from "firebase/firestore"
+import {CheckIcon, Cross1Icon, Pencil1Icon} from "@radix-ui/react-icons"
+import {useForm} from "react-hook-form"
 
-export default function ProfilePage({bgUser, db}: { bgUser: BGUser | null, db: Firestore | null }) {
-    const [currentUserName, setCurrentUserName] = useState<string>("")
-    const [currentBGGUsername, setCurrentBGGUsername] = useState<string>("")
+type UsernameFormValues = {
+    username: string
+}
+
+type BGGUsernameFormValues = {
+    bggUsername: string
+}
+
+export default function ProfilePage({bgUser, setBgUser, db}: {
+    bgUser: BGUser | null,
+    setBgUser: Dispatch<SetStateAction<BGUser | null>>,
+    db: Firestore | null
+}) {
     const [editingUsername, setEditingUsername] = useState<boolean>(false)
     const [editingBGGUsername, setEditingBGGUsername] = useState<boolean>(false)
+
+    const {
+        handleSubmit: handleSubmitUsername,
+        register: registerUsername,
+        reset: resetUsername,
+        formState: {errors: errorsUsername}
+    } = useForm<UsernameFormValues>({mode: "onChange"})
+    const {
+        handleSubmit: handleSubmitBGGUsername,
+        register: registerBGGUsername,
+        reset: resetBGGUsername,
+        formState: {errors: errorsBGGUsername}
+    } = useForm<BGGUsernameFormValues>({mode: "onChange"})
 
     function editUsername() {
         setEditingUsername(true)
@@ -17,38 +42,33 @@ export default function ProfilePage({bgUser, db}: { bgUser: BGUser | null, db: F
         setEditingBGGUsername(true)
     }
 
-    async function saveUsername() {
+    async function saveUsername(data: UsernameFormValues) {
         try {
-            if (db && bgUser) {
+            if (db && bgUser && bgUser.username !== data.username) {
                 const userDocRef = doc(db, "users", bgUser.uid)
-                await updateDoc(userDocRef, {username: currentUserName})
-                setEditingUsername(false)
+                await updateDoc(userDocRef, {username: data.username})
+                setBgUser({...bgUser, username: data.username})
             }
+            setEditingUsername(false)
+            resetUsername(data)
         } catch (e) {
             console.error("Error saving username:", e)
         }
     }
 
-    async function saveBGGUsername() {
+    async function saveBGGUsername(data: BGGUsernameFormValues) {
         try {
-            if (db && bgUser) {
+            if (db && bgUser && bgUser.bggUsername !== data.bggUsername) {
                 const userDocRef = doc(db, "users", bgUser.uid)
-                await updateDoc(userDocRef, {bggUsername: currentBGGUsername})
-                setEditingBGGUsername(false)
+                await updateDoc(userDocRef, {bggUsername: data.bggUsername})
+                setBgUser({...bgUser, bggUsername: data.bggUsername})
             }
+            setEditingBGGUsername(false)
+            resetBGGUsername(data)
         } catch (e) {
-            console.error("Error saving username:", e)
+            console.error("Error saving BGG username:", e)
         }
     }
-
-    useEffect(() => {
-        if (bgUser?.username) {
-            setCurrentUserName(bgUser.username)
-        }
-        if (bgUser?.bggUsername) {
-            setCurrentBGGUsername(bgUser.bggUsername)
-        }
-    }, [bgUser?.bggUsername, bgUser?.username])
 
     return (
         <div className={"profile-page"}>
@@ -56,39 +76,100 @@ export default function ProfilePage({bgUser, db}: { bgUser: BGUser | null, db: F
                 <>
                     <h2 className={"profile-name-image"}>
                         {bgUser.photoURL && (
-                            <img className={"profile-image"} src={bgUser.photoURL}
-                                 alt={"Photo of " + bgUser.displayName}/>
+                            <img className={"profile-image"}
+                                 src={bgUser.photoURL}
+                                 alt={"Photo of " + bgUser.displayName}
+                            />
                         )}
                         {bgUser.displayName}
                     </h2>
-                    <div className={"profile-grid"}>
-                        <div className={"field-name"}>
-                            Username:
-                        </div>
-                        <input
-                            type={"text"}
-                            value={currentUserName}
-                            onChange={(e) => setCurrentUserName(e.target.value)}
-                            disabled={!editingUsername}/>
-                        {editingUsername ? (
-                            <button onClick={saveUsername}>Save</button>
-                        ) : (
-                            <button onClick={editUsername}>Edit</button>
-                        )}
-                        <div className={"field-name"}>
-                            BoardGameGeek <br/> Username:
-                        </div>
-                        <input
-                            type={"text"}
-                            value={currentBGGUsername}
-                            onChange={(e) => setCurrentBGGUsername(e.target.value)}
-                            disabled={!editingBGGUsername}/>
-                        {editingBGGUsername ? (
-                            <button onClick={saveBGGUsername}>Save</button>
-                        ) : (
-                            <button onClick={editBGGUsername}>Edit</button>
-                        )}
-
+                    <div>
+                        <form className={"profile-grid"} onSubmit={handleSubmitUsername(saveUsername)}>
+                            <label className={"field-name"} htmlFor={"username"}>Username:</label>
+                            {editingUsername ? (
+                                <>
+                                    <div>
+                                        <div className={"field-error-placeholder"}>
+                                            {errorsUsername.username && (
+                                                <div className={"field-error"}>{errorsUsername.username.message}</div>
+                                            )}
+                                        </div>
+                                        <input
+                                            id={"username"}
+                                            className={"field-input"}
+                                            type={"text"}
+                                            defaultValue={bgUser.username}
+                                            {...registerUsername("username", {
+                                                maxLength: {
+                                                    value: 20,
+                                                    message: "max 20 characters"
+                                                }
+                                            })}
+                                        />
+                                    </div>
+                                    <div className={"field-buttons"}>
+                                        <button className={"profile-button"} type={"submit"}><CheckIcon/></button>
+                                        <button className={"profile-button"} onClick={() => resetUsername()}>
+                                            <Cross1Icon/>
+                                        </button>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <div>{bgUser.username}</div>
+                                    <button className={"profile-button"} onClick={editUsername}><Pencil1Icon/></button>
+                                </>
+                            )}
+                        </form>
+                        <form className={"profile-grid"} onSubmit={handleSubmitBGGUsername(saveBGGUsername)}>
+                            <label className={"field-name"}
+                                   htmlFor={"bggUsername"}
+                            >
+                                BoardGameGeek <br/> Username:
+                            </label>
+                            {editingBGGUsername ? (
+                                <>
+                                    <div>
+                                        <div className={"field-error-placeholder"}>
+                                            {errorsBGGUsername.bggUsername && (
+                                                <div
+                                                    className={"field-error"}
+                                                >
+                                                    {errorsBGGUsername.bggUsername.message}
+                                                </div>
+                                            )}
+                                        </div>
+                                        <input
+                                            id={"bggUsername"}
+                                            className={"field-input"}
+                                            type={"text"}
+                                            defaultValue={bgUser.bggUsername}
+                                            {...registerBGGUsername("bggUsername", {
+                                                maxLength: {
+                                                    value: 20,
+                                                    message: "max 20 characters"
+                                                }, pattern: {
+                                                    value: /^\w+$/,
+                                                    message: "unsupported characters"
+                                                }
+                                            })}
+                                        />
+                                    </div>
+                                    <div className={"field-buttons"}>
+                                        <button className={"profile-button"} type={"submit"}><CheckIcon/></button>
+                                        <button className={"profile-button"} onClick={() => resetBGGUsername()}>
+                                            <Cross1Icon/>
+                                        </button>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <div>{bgUser.bggUsername}</div>
+                                    <button className={"profile-button"} onClick={editBGGUsername}><Pencil1Icon/>
+                                    </button>
+                                </>
+                            )}
+                        </form>
                     </div>
                 </>
             ) : (
